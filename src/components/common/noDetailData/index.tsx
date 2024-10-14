9ea07d7d375c0f476b034148fe6c006540e23fbd
yhs0329 <scar9983@gamil.com>
2024-10-14 16:25:24 +0900

    css 수정

--- src/components/common/noDetailData/index.tsx
+++ src/components/common/noDetailData/index.tsx
@@ -5,8 +5,8 @@ const cn = classNames.bind(styles);
 const NodetailData = () => {
   return (
     <div className={cn('container')}>
-      <span>답지가 없어요</span>
-      <span> 회원님만의 답지를 공유해 주세요🙌</span>
+      <span>아직 답지가 없네요!</span>
+      <span> 회원님의 답지를 공유해보세요💡</span>
     </div>
   );
 };
