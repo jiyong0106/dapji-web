92fe29638203cbb8d48bd81ed27b3796559a2b9b
jiyong <parkjiy96@naver.com>
2024-12-10 20:43:17 +0900

    feat : 댓글 작성시 세션 없으면 리다이렉트

--- src/hooks/useCommentDatas.ts
+++ src/hooks/useCommentDatas.ts
@@ -5,6 +5,8 @@ import {
 } from '@tanstack/react-query';
 import instance from '../utils/axios';
 import { useModal } from './useModal';
+import { isServerError } from '@/src/utils/axiosError';
+import { useRouter } from 'next/navigation';
 
 // 댓글 조회
 
@@ -44,6 +46,7 @@ export const useCommentUploadData = ({
 }: useCommentUploadDataProps) => {
   const queryClient = useQueryClient();
   const { showModalHandler } = useModal();
+  const router = useRouter();
 
   return useMutation({
     mutationKey: [`${mainKey}`],
@@ -52,8 +55,14 @@ export const useCommentUploadData = ({
       queryClient.invalidateQueries({ queryKey: [`${firKey}`] }),
       queryClient.invalidateQueries({ queryKey: [`${secKey}`] }),
     ],
-    onError: () => {
+    onError: (e) => {
       showModalHandler('alert', '댓글을 다시 업로드해 주세요');
+      if (isServerError(e) && e.response && e.response.status === 401) {
+        showModalHandler('alert', '댓글을 작성하려면 로그인이 필요해요', () =>
+          router.replace('/'),
+        );
+        return;
+      }
     },
   });
 };
@@ -145,6 +154,7 @@ export const useRecommentUploadData = ({
 }: useRecommentUploadDataProps) => {
   const queryClient = useQueryClient();
   const { showModalHandler } = useModal();
+  const router = useRouter();
 
   return useMutation({
     mutationKey: [`${mainKey}`],
@@ -153,8 +163,14 @@ export const useRecommentUploadData = ({
       queryClient.invalidateQueries({ queryKey: [`${firKey}`] }),
       queryClient.invalidateQueries({ queryKey: [`${secKey}`] }),
     ],
-    onError: () => {
+    onError: (e) => {
       showModalHandler('alert', '답글을 다시 업로드해 주세요');
+      if (isServerError(e) && e.response && e.response.status === 401) {
+        showModalHandler('alert', '답글을 작성하려면 로그인이 필요해요', () =>
+          router.replace('/'),
+        );
+        return;
+      }
     },
   });
 };
