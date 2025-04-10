8acd3c2e8f61c6c305e5aabd5f245d4fd43b1971
jiyong <parkjiy96@naver.com>
2025-04-10 20:45:06 +0900

    프로필의 암장 상세페이지 구현

--- src/app/(main)/profile/[userId]/[gymId]/page.tsx
+++ src/app/(main)/profile/[userId]/[gymId]/page.tsx
@@ -4,6 +4,8 @@ import styles from './profileGymPage.module.scss';
 import classNames from 'classnames/bind';
 import { ProfileGymsDetailResponseType } from '@/src/utils/type';
 import { fetchProfileGymDetail } from '../../api';
+import ProfileGymDetailDatas from '@/src/components/profilePage/profileGymDetailDatas.tsx';
+import Image from 'next/image';
 
 const cn = classNames.bind(styles);
 
@@ -33,12 +35,32 @@ const ProfileGymPage = ({ params }: ProfilePageProps) => {
 
   const gymDetailDatas = profileGymData?.pages[0].gym;
 
-  const postDetailDatas = profileGymData?.pages.flatMap((page) => page.posts);
+  const postDetailDatas =
+    profileGymData?.pages.flatMap((page) => page.posts) ?? [];
 
   return (
     <div className={cn('container')}>
-      <p>{userId}</p>
-      <p>{gymId}</p>
+      <div className={cn('gymInfo')}>
+        <div className={cn('imageWrapper')}>
+          <Image
+            src={
+              gymDetailDatas?.logo ||
+              process.env.NEXT_PUBLIC_URL + '/icon/icon.png'
+            }
+            alt="로고이미지"
+            fill
+            className={cn('image')}
+            priority
+          />
+        </div>
+        <div className={cn('gymInfoText')}>
+          <h4>{gymDetailDatas?.name}</h4>
+          <p>{gymDetailDatas?.address}</p>
+          <p>총 {gymDetailDatas?.post_count}개의 답지를 올렸어요</p>
+        </div>
+      </div>
+      <ProfileGymDetailDatas items={postDetailDatas} gymId={gymId} />
+      <div ref={ref} />
     </div>
   );
 };
---
+++ src/components/profilePage/profileGymDetailDatas.tsx/index.tsx
@@ -0,0 +1,77 @@
+'use client';
+import React from 'react';
+import styles from './profileGymDetailDatas.module.scss';
+import classNames from 'classnames/bind';
+import { ProfileGymDetailPostType } from '@/src/utils/type';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+const cn = classNames.bind(styles);
+
+type ProfileGymDetailDatapProps = {
+  item: ProfileGymDetailPostType;
+  gymId: string;
+};
+
+const ProfileGymDetail = ({ item, gymId }: ProfileGymDetailDatapProps) => {
+  const { post_idx, thumbnailUrl, clearday, color, media_count } = item;
+  const router = useRouter();
+  const dateOnly = clearday.split('T')[0];
+
+  const postClick = () => {
+    router.push(`/gym/${gymId}/${post_idx}`);
+  };
+
+  return (
+    <div className={cn('container')} onClick={postClick}>
+      <div className={cn('imageWrapper')}>
+        <Image
+          src={thumbnailUrl || process.env.NEXT_PUBLIC_URL + '/icon/icon.png'}
+          alt="로고이미지"
+          fill
+          className={cn('image')}
+          priority
+        />
+      </div>
+      <div className={cn('postInfoText')}>
+        <p>{dateOnly}</p>
+        <div className={cn('colorWrapper')}>
+          <div
+            className={cn('color')}
+            style={{
+              color: color,
+              border:
+                color.toLowerCase() === 'white'
+                  ? '0.5px solid #858585'
+                  : 'none',
+            }}
+          />
+          <span>{media_count}개</span>
+          <span>|</span>
+          <span>답지로 이동</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+////////
+type ProfileGymDetailDataspProps = {
+  items: ProfileGymDetailPostType[];
+  gymId: string;
+};
+
+const ProfileGymDetailDatas = ({
+  items,
+  gymId,
+}: ProfileGymDetailDataspProps) => {
+  return (
+    <div className={cn('outContainer')}>
+      {items.map((item) => (
+        <ProfileGymDetail key={item.post_idx} item={item} gymId={gymId} />
+      ))}
+    </div>
+  );
+};
+
+export default ProfileGymDetailDatas;
