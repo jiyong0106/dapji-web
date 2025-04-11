98d2a34d43c53f7c836ba11a8843e5b37848b446
jiyong <parkjiy96@naver.com>
2025-04-11 16:09:33 +0900

    랜딩 howwork섹션 문구 수정

--- src/components/landingPage/mainSection/index.tsx
+++ src/components/landingPage/mainSection/index.tsx
@@ -29,7 +29,7 @@ const MainSection = () => {
     <section id="brand" ref={ref} className={cn('container')}>
       <div className={cn('wrapper', { animate: isVisible })}>
         <div className={cn('left')}>
-          <h2 className={cn('slogan')}>클라이밍 루트, 너무 고민하지 마세요</h2>
+          <h2 className={cn('slogan')}>루트 파인딩, 너무 고민하지 마세요</h2>
           <p className={cn('desc')}>
             루트가 안 보일 때, 다른 클라이머의 완등 영상은 최고의 힌트!
             <br />
