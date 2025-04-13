868aa9623a5668224e52c9e64f8d9e28412ac2bb
jiyong <parkjiy96@naver.com>
2025-04-13 15:28:58 +0900

    랜딩페이지 텍스트 줄바꿈 수정

--- src/utils/options/landingOptions.ts
+++ src/utils/options/landingOptions.ts
@@ -69,7 +69,7 @@ export const landingHowOptions = [
   {
     icon: '/images/landingFilter.png',
     title: '쉽고 빠른 답지 검색',
-    desc: '원하는 클라이밍장 선택하고, 원하는 난이도 선택하면 끝!',
+    desc: '보고싶은 클라이밍장과 찾는 난이도 선택하면 끝!',
     features: [
       '홀드 색상별로 답지 찾기',
       '보고싶은 문제가 있는지 확인',
@@ -79,7 +79,7 @@ export const landingHowOptions = [
   {
     icon: '/images/landingUpload.png',
     title: '답지 업로드',
-    desc: '나만의 클라이밍 영상, 답지를 업로드하고 다른 사람들에게 자랑해보세요',
+    desc: '나만의 클라이밍 영상, 답지를 업로드하고\n다른 사람들에게 자랑해보세요',
     features: [
       '대용량 영상도 문제없이!',
       '직접 촬영한 영상 및 인스타그램 링크로 업로드',
@@ -90,7 +90,7 @@ export const landingHowOptions = [
   {
     icon: '/images/landingCalendar.png',
     title: '클라이밍 기록과 일정 관리',
-    desc: '운동 기록과 일정을 한 눈에 볼 수 있어요. 내가 언제, 어디서 얼마나 클라이밍을 했는지 한 번에 정리돼요.',
+    desc: '운동 기록과 일정을 한 눈에 볼 수 있어요. 내가 언제,\n어디서 얼마나 클라이밍을 했는지 한 번에 정리돼요.',
     features: [
       '답지를 업로드하면 자동으로 기록',
       '답지가 없어도 직접 기록 추가 가능',
@@ -101,7 +101,7 @@ export const landingHowOptions = [
   {
     icon: '/images/landingProfile.png',
     title: '프로필 꾸미기',
-    desc: '키와 리치부터 등반 스타일, 영상까지! 나만의 클라이밍 프로필을 만들고, 성장 과정을 자연스럽게 보여줄 수 있어요.',
+    desc: '키와 리치부터 등반 스타일, 영상까지! 나만의 클라이밍\n프로필을 만들고, 성장 과정을 볼 수 있어요.',
     features: [
       '자기소개 및 클라이밍 목표 설정',
       '내가 클리어 한 문제 한눈에 보기',
@@ -112,7 +112,7 @@ export const landingHowOptions = [
   {
     icon: '/images/landingCommu.png',
     title: '답지 커뮤니티',
-    desc: '운동 얘기부터 장비 추천, 오늘의 TMI까지! 클라이머들끼리 모여 자유롭게 소통해요. 답지는 아지트 그 자체.',
+    desc: '운동 얘기부터 장비 추천, TMI까지! 클라이머들끼리\n모여 자유롭게 소통해요. 답지는 아지트 그 자체.',
     features: [
       '질문, 정보, 잡담 등 자유게시판 운영',
       '주제별 카테고리 필터링 (장비, 부상, 암장, 훈련 등)',
