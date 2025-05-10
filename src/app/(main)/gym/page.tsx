651007e448d31c20463142d16e206f9fc15b59f4
jiyong <parkjiy96@naver.com>
2025-05-10 18:04:40 +0900

    클라이밍장 리스트 페이지 generateMetadata title  변경

--- src/app/(main)/gym/page.tsx
+++ src/app/(main)/gym/page.tsx
@@ -3,11 +3,11 @@ import instance from '@/src/utils/axios';
 
 export const generateMetadata = async () => {
   return {
-    title: '클라이밍장 리스트',
-    description: '클라이밍장 리스트',
+    title: '클라이밍장 목록',
+    description: '클라이밍장 목록',
     openGraph: {
-      title: '클라이밍장 리스트',
-      description: '클라이밍장 리스트',
+      title: '클라이밍장 목록',
+      description: '클라이밍장 목록',
       url: `${process.env.NEXT_PUBLIC_URL}/gym`,
       images: '/icon/widelogo.png',
     },
